d3e307785562dc96955639560c0d06bc1123787f
ari <aniknia.dev@gmail.com>
2022-06-03 20:29:33 -0700

    test: testing api

--- components/main.tsx
+++ components/main.tsx
@@ -1,7 +1,16 @@
 import { Flex } from "@chakra-ui/react";
 import Item from "./item";
+import { useEffect } from "react";
 
 export default function Main() {
+  useEffect(() => {
+    fetch("https://api.petrarchive.io/api/petrs")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }, []);
+
   return (
     <Flex justify="space-around" wrap="wrap">
       <Item name="Petr" author="Petr" />
