f64502077f96bd399c54233b3ec2847d2051594d
aniknia <aniknia.dev@gmail.com>
2022-06-06 02:48:52 -0700

    fix: add aria label to likes component

--- components/like.tsx
+++ components/like.tsx
@@ -4,7 +4,7 @@ import { HeartFillIcon } from "@primer/octicons-react";
 export default function Like(props) {
   return (
     <>
-      <IconButton icon={<HeartFillIcon size={16} />} />
+      <IconButton aria-label="Like Petr" icon={<HeartFillIcon size={16} />} />
       <Text>{props.likes}</Text>
     </>
   );
