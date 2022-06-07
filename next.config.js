fa2146633c8b6f1c43c42153412a30b9c453ae8e
ari <aniknia.dev@gmail.com>
2022-06-06 19:13:03 -0700

    fix: fix typo

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
   },
   env: {
     API_HOST: process.env.API_HOST,
-    API_KEY: process.enc.API_KEY,
+    API_KEY: process.env.API_KEY,
   },
 };
 module.exports = nextConfig;
