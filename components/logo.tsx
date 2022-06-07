73a4ffe1567bff0f7081efe0729c204e2d4b9263
ari <aniknia.dev@gmail.com>
2022-06-07 04:17:12 -0700

    feat: add new site logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,9 +1,12 @@
 import { HStack } from "@chakra-ui/react";
+import Image from "../node_modules/next/image";
+import logo_santa from "../public/images/logo_santa.png";
 
 export default function Logo() {
   return (
     <HStack>
-      <p>Petr Archive</p>
+      <Image src={logo_santa} alt="Petr" width={55} height={40} />
+      <p>Archive</p>
     </HStack>
   );
 }
