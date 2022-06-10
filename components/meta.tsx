eda53b3d6ffe2f6d9309b9bba18d1b510011cc54
ari <aniknia.dev@gmail.com>
2022-06-10 07:05:42 -0700

    feat: add umami script for data analytics

--- components/meta.tsx
+++ components/meta.tsx
@@ -7,6 +7,12 @@ export default function Meta() {
         <title>Petr Archive</title>
         <meta name="description" content="An archive of the UCI Petrs" />
         <link rel="icon" href="favicon.ico" />
+        <script
+          async
+          defer
+          data-website-id="b1a27733-c8c6-44f8-b966-edbb68af40db"
+          src="https://analytics.petrarchive.io/umami.js"
+        ></script>
       </Head>
     </>
   );
