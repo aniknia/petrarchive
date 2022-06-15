c779afbdb3a0f78d745946b35bdd8577d571ee24
ari <aniknia.dev@gmail.com>
2022-06-15 00:39:02 -0700

    fix: moved umami script into a nextjs script component

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,4 +1,5 @@
 import Head from "../node_modules/next/head";
+import Script from "../node_modules/next/script";
 
 export default function Meta() {
   return (
@@ -7,13 +8,13 @@ export default function Meta() {
         <title>Petr Archive</title>
         <meta name="description" content="An archive of the UCI Petrs" />
         <link rel="icon" href="favicon.ico" />
-        <script
-          async
-          defer
-          data-website-id="a6b6959f-c861-405e-b3af-2114df1e0e7f"
-          src="https://analytics.petrarchive.io/umami.js"
-        ></script>
       </Head>
+      <Script
+        async
+        defer
+        data-website-id="a6b6959f-c861-405e-b3af-2114df1e0e7f"
+        src="https://analytics.petrarchive.io/umami.js"
+      ></Script>
     </>
   );
 }
