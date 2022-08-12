51e91d561135bf14225a5ee6ac8aacb5ebb62bab
ari <aniknia.dev@gmail.com>
2022-08-12 00:52:26 -0700

    feat: test update api of likes

--- components/like.tsx
+++ components/like.tsx
@@ -1,29 +1,70 @@
 import { IconButton } from "@chakra-ui/react";
 import { HeartFillIcon } from "@primer/octicons-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CountUp from "react-countup";
+import {
+  getCookies,
+  setCookies,
+  checkCookies,
+  removeCookies,
+} from "cookies-next";
+import PetrProvider from "./petrprovider";
+
+// TODO: likes counter starts at a dcrement of one, fix this
+// TODO: implement a cleaner cookies system
 
 export default function Like(props) {
   const [likes, setLikes] = useState(props.likes);
-  const [lastLike, setLastLike] = useState(0);
+  const [likeState, setLikeState] = useState(verifyCookies());
+  const [lastLikeState, setLastLikeState] = useState(verifyCookies());
   const [duration, setDuration] = useState(1.5);
 
-  function increment() {
-    setLastLike(likes);
-    setLikes(likes + 1);
-    setDuration(0.1);
+  function verifyCookies() {
+    if (checkCookies(props.id)) {
+      return getCookies(props.id)[props.id] === "true" ? true : false;
+    } else {
+      setCookies(props.id, false);
+      return getCookies(props.id)[props.id] === "true" ? true : false;
+    }
   }
+
+  function updateLikes(likeState: boolean) {
+    fetch(process.env.API_HOST + "/api/petrs/" + props.id)
+      .then((response) => response.json())
+      .then((data) => {
+        fetch(process.env.API_HOST + "/api/petrs/" + props.id, {
+          method: "PUT",
+          body: JSON.stringify({
+            data: {
+              likes: (data.data.attributes.likes = likeState
+                ? data.data.attributes.likes + 1
+                : data.data.attributes.likes - 1),
+            },
+          }),
+        }).then((response) => response.json());
+      });
+  }
+
+  useEffect(() => {
+    if (likeState !== lastLikeState) {
+      updateLikes(likeState);
+      likeState ? setLikes(likes + 1) : setLikes(likes - 1);
+      setCookies(props.id, likeState ? true : false);
+      setLastLikeState(likeState);
+    }
+    setDuration(0.1);
+  }, [likeState]);
 
   return (
     <>
-      <CountUp start={lastLike} end={likes} duration={duration} />
+      <CountUp start={lastLikeState} end={likes} duration={duration} />
 
       <IconButton
-        color="pink"
+        color={likeState ? "red" : "pink"}
         aria-label="Like Petr"
         icon={<HeartFillIcon size={24} />}
         isRound={true}
-        onClick={() => increment()}
+        onClick={() => setLikeState(!likeState)}
       />
     </>
   );
