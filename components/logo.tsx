05fe28963ac8768d7c60bd91515d24e38cb0ab9e
ari <aniknia.dev@gmail.com>
2022-10-12 16:06:15 -0700

    refactor: use switch instead of if for faster loads

--- components/logo.tsx
+++ components/logo.tsx
@@ -5,24 +5,49 @@ import merry_petr from "../public/images/seasonal/Merry_Petr.png";
 import spooky_petr from "../public/images/seasonal/Spooky_Petr.png";
 import { useEffect, useState } from "react";
 
-class LogoObj {
-  name: string;
-  image: string;
-  month: number;
-}
-
 export default function Logo() {
-  const [logo, setLogo] = useState(new Array<LogoObj>());
   const [source, setSource] = useState(petr);
   let date = new Date();
   let month = date.getMonth() + 1;
 
   useEffect(() => {
-    if (month == 10) {
-      setSource(spooky_petr);
-    }
-    if (month == 12) {
-      setSource(merry_petr);
+    switch (month) {
+      case 1:
+        setSource(petr);
+        break;
+      case 2:
+        setSource(petr);
+        break;
+      case 3:
+        setSource(petr);
+        break;
+      case 4:
+        setSource(petr);
+        break;
+      case 5:
+        setSource(petr);
+        break;
+      case 6:
+        setSource(petr);
+        break;
+      case 7:
+        setSource(petr);
+        break;
+      case 8:
+        setSource(petr);
+        break;
+      case 9:
+        setSource(petr);
+        break;
+      case 10:
+        setSource(spooky_petr);
+        break;
+      case 11:
+        setSource(petr);
+        break;
+      case 12:
+        setSource(merry_petr);
+        break;
     }
   }, [month]);
 
