41afa350d801a580c6c15d4ce0b476f25c4ceb35
ari <aniknia.dev@gmail.com>
2022-10-20 13:27:05 -0700

    chore: adding  visuals for searchbar and sort button

--- components/main.tsx
+++ components/main.tsx
@@ -1,4 +1,6 @@
 import { Flex } from "@chakra-ui/react";
+import Searchbar from "./searchbar";
+
 import Item from "./item";
 import { useContext } from "react";
 import { PetrContext } from "./petrprovider";
@@ -7,15 +9,19 @@ export default function Main() {
   const value = useContext(PetrContext);
 
   return (
-    <Flex justify="space-around" wrap="wrap">
-      {value.petrs &&
-        value.petrs
-          .sort((a, b) => b.created.getTime() - a.created.getTime())
-          .map((item) => {
-            if (item.dropped) {
-              return <Item key={item.id} petr={item} />;
-            }
-          })}
-    </Flex>
+    <>
+      {" "}
+      <Searchbar />
+      <Flex justify="space-around" wrap="wrap">
+        {value.petrs &&
+          value.petrs
+            .sort((a, b) => b.created.getTime() - a.created.getTime())
+            .map((item) => {
+              if (item.dropped) {
+                return <Item key={item.id} petr={item} />;
+              }
+            })}
+      </Flex>
+    </>
   );
 }
