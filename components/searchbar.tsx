303bf7b9e85f1b293a14aa1bc462269459720c0f
ari <aniknia.dev@gmail.com>
2023-02-07 16:31:23 -0800

    refactor: remove searchbar

--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -12,6 +12,8 @@ import {
 } from "@chakra-ui/react";
 import { SearchIcon, XIcon } from "@primer/octicons-react";
 
+// TODO: Reimplement the searchbar
+
 export default function Searchbar() {
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
@@ -20,21 +22,27 @@ export default function Searchbar() {
     <>
       <Stack>
         <VStack gap="2" pt="2" pl="7" pr="7">
-          <InputGroup size="lg" minW="320">
-            <InputLeftElement pointerEvents="none">
-              <SearchIcon key="1" size={24} />
-            </InputLeftElement>
+          {show ? (
+            <>
+              <InputGroup size="lg" minW="320">
+                <InputLeftElement pointerEvents="none">
+                  <SearchIcon key="1" size={24} />
+                </InputLeftElement>
 
-            <Input
-              variant="filled"
-              placeholder="Search for your favourite Petr"
-            />
-            <InputRightElement>
-              <Button variant="none" onClick={handleClick}>
-                <XIcon size={24} />
-              </Button>
-            </InputRightElement>
-          </InputGroup>
+                <Input
+                  variant="filled"
+                  placeholder="Search for your favourite Petr"
+                />
+                <InputRightElement>
+                  <Button variant="none" onClick={handleClick}>
+                    <XIcon size={24} />
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </>
+          ) : (
+            <></>
+          )}
 
           <Box p="1" minW="130">
             <Select variant="filled" placeholder="Newest">
