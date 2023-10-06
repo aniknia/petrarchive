62e3273de629d613f34d4fb7fd81b40e6d1ef928
ari <aniknia.dev@gmail.com>
2023-10-06 11:18:14 -0700

    fix: change colorswitch button size and made settings an iconbutton

--- components/general/settingsnavbutton.tsx
+++ components/general/settingsnavbutton.tsx
@@ -1,19 +1,25 @@
-import { Button } from "@chakra-ui/react";
+import { IconButton } from "@chakra-ui/react";
 import { useContext, useEffect, useState } from "react";
 import Link from "../../node_modules/next/link";
 import { AccountContext } from "../provider/accountprovider";
+import { GearIcon } from "@primer/octicons-react";
+import { useRouter } from "../../node_modules/next/router";
 
 
 export default function SettingsNavButton() {
     const loginContext = useContext(AccountContext);
+    const router = useRouter();
+
+    function redirect() {
+        router.push('/settings')
+    }
 
     useEffect(() => { }, [loginContext]);
 
     return (
         <>
-            <Button size="md" fontSize="lg" variant="ghost" color="current">
-                <Link href="/settings">Settings</Link>
-            </Button>
+            <IconButton size="md" fontSize="lg" variant="ghost" color="current" icon={<GearIcon size={24} />} onClick={() => redirect()}>
+            </IconButton>
 
         </>
     );
